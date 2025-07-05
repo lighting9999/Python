3ef5ac158e008abcc41cc6289d9eebe788995dc2
lighting9999 <lighting0909@yeah.net>
2025-07-05 17:49:01 +0800

    Update import current_stock_price.py

--- web_programming/current_stock_price.py
+++ web_programming/current_stock_price.py
@@ -5,12 +5,9 @@
 #     "httpx",
 # ]
 # ///
-
-from doctest import testmod
-
-from bs4 import BeautifulSoup
 import requests
-
+from bs4 import BeautifulSoup
+from doctest import testmod
 """
 Get the HTML code of finance yahoo and select the current qsp-price
 Current AAPL stock price is   228.43
